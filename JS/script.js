07691fadc4f1d1bd317f2568df4e8740902c9f3e
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-06 14:32:13 +0200

    [UPDATE]: file script

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+/*
+             Password generator
+   ========================================          
+
+1. Salvare nelle variabili il prompt del nome, cognome e colore preferito;
+
+2. Salvare il messaggio in una variabile;
+
+3. Stampare sulla pagina il valore della variabile del messaggio.
+
+*/
+
+
