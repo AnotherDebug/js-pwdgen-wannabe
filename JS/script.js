aae84d412bf8a5341c734e2e5adab35d00cacb37
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-06 15:10:08 +0200

    [UPDATE]: file script

--- JS/script.js
+++ JS/script.js
@@ -11,3 +11,26 @@
 */
 
 
+
+
+
+// 1.
+const firstName = prompt('Inserisci il tuo Nome');
+
+
+const lastName = prompt('Inserisci il tuo Cognome');
+
+
+const favColor = prompt('Inserisci il tuo colore preferito');
+
+
+// 2.
+
+const message = firstName + lastName + favColor + '23';
+
+const messagestr = document.getElementById('output');
+
+
+// 3.
+
+messagestr.innerHTML = message.toLowerCase(message);
